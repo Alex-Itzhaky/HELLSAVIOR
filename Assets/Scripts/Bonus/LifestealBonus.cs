c05153f27bf7a7bc744a317dd3c6bf98c7fdf107
Lassalle2027 <alexitzhaky@gmail.com>
2026-05-31 21:59:50 +0200

    Build marche

--- Assets/Scripts/Bonus/LifestealBonus.cs
+++ Assets/Scripts/Bonus/LifestealBonus.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class LifestealBonus : BonusItems
 {
     [SerializeField] private float lifestealDuration;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private CircleCollider2D circleCollider;
 
     public override void OnPickup()
     {
@@ -13,9 +15,12 @@ public class LifestealBonus : BonusItems
     private IEnumerator LifestealCoroutine(float lifestealDuration)
     {
         player.GetComponent<HealthController>().isLifestealBonusEnabled = true;
+        spriteRenderer.enabled = false;
+        circleCollider.enabled = false;
         yield return new WaitForSeconds(lifestealDuration);
         player.GetComponent<HealthController>().isLifestealBonusEnabled = false;
         Destroy(gameObject);
+        
     }
 
 }
